1ebddf64ac04e13f18dd548517fb0fc78d91d803
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-09-07 18:16:28 -0700

    Add boilerplate to index.js (renaming server.js), add package.json

---
+++ index.js
@@ -0,0 +1,21 @@
+// Include packages
+const inquirer = require('inquirer');
+
+// User input questions
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'projname',
+    }
+]
+
+// Function to initialize the application
+function init () {
+    inquirer.prompt(questions).then((response) => {
+        console.log(response);
+    });
+}
+
+// Initialize the application
+init();
