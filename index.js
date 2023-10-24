d950596e1acca6add43f199d3a1ea95b51226e48
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-10-24 13:14:06 -0700

    Add initial menu to index.js

--- index.js
+++ index.js
@@ -5,18 +5,27 @@ const mysql = require('mysql2');
 // User input questions
 const questions = [
     {
-        type: 'input',
-        message: 'What is the name of your project?',
-        name: 'projname',
+        type: 'list',
+        message: 'Please choose from the following options:',
+        name: 'initialChoice',
+        choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
     }
 ]
 
 // Function to initialize the application
 function init () {
     inquirer.prompt(questions).then((response) => {
-        console.log(response);
+        //console.log(response);
+        if (response.choices = "view all departments") {
+            console.log("view all departments");
+        }
     });
 }
 
 // Initialize the application
 init();
+
+// VIEW ALL DEPARTMENTS
+function viewAllDepartments() {
+
+}
