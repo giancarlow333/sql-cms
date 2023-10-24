65b94886d6be641105ca4d9ee5ec5ce7a40ebfa4
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-10-24 13:20:14 -0700

    Attempt a viewAllDepts function

--- index.js
+++ index.js
@@ -2,6 +2,17 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    console.log(`Connected to the employee_db database.`)
+);
+
 // User input questions
 const questions = [
     {
@@ -17,8 +28,10 @@ function init () {
     inquirer.prompt(questions).then((response) => {
         //console.log(response);
         if (response.choices = "view all departments") {
-            console.log("view all departments");
+            console.log("chose \"view all departments\"");
+            viewAllDepartments();
         }
+        console.log("chose something else");
     });
 }
 
@@ -27,5 +40,7 @@ init();
 
 // VIEW ALL DEPARTMENTS
 function viewAllDepartments() {
-
+    db.query('SELECT * FROM department', function (err, results) {
+        console.log(results);
+    });
 }
