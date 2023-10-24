9d6dfa13f73f01074b20c2d29d6cb51a0344fbb2
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-10-24 13:31:41 -0700

    Fix logic error in inquirer

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ const db = mysql.createConnection(
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME
-    }
+    },
+    console.log(`Connected to the employee_db database.`)
 );
-db.connect(function(err) {
+/*db.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
 }); //https://www.w3schools.com/nodejs/nodejs_mysql.asp */
@@ -25,20 +26,20 @@ const questions = [
         name: 'initialChoice',
         choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role'],
     }
-]
+];
 
 // Function to initialize the application
 function init () {
     inquirer.prompt(questions).then((response) => {
-        //console.log(response);
-        if (response.choices = "view all departments") {
+        console.log(response);
+        if (response.initialChoice == "view all departments") {
             console.log("chose \"view all departments\"");
             viewAllDepartments();
         }
         else {console.log("chose something else");}
         return;
     });
-}
+};
 
 // Initialize the application
 init();
